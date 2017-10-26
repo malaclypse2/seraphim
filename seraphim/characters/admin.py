6a9f32ebae7b8d353da09c5635c0a22d21c7d760
unknown <malaclypse2@gmail.com>
2017-10-25 22:09:28 -0400

    admin

--- seraphim/characters/admin.py
+++ seraphim/characters/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Character
 admin.site.register(Character)
+
+from .models import CharacterIcon
+admin.site.register(CharacterIcon)
