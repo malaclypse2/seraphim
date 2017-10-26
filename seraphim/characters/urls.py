9f969f176874d48b43207ac013ba4a1f79ee0ff1
unknown <malaclypse2@gmail.com>
2017-10-25 21:41:23 -0400

    starting character editors

--- seraphim/characters/urls.py
+++ seraphim/characters/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # e.g.: /
     url(r'^$', views.index, name='index'),
+    # e.g.: /characters/3/
+    url(
+        regex=r'^(?P<character_id>[0-9]+)/$',
+        view=views.CharacterDetailView.as_view(),
+        name='detail'
+    ),
 ]
--- seraphim/characters/views.py
+++ seraphim/characters/views.py
@@ -1,5 +1,18 @@
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView, RedirectView, UpdateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Character
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the characters index.")
+
+class CharacterDetailView(LoginRequiredMixin, DetailView):
+    model = Character
+
+class CharacterListView(LoginRequiredMixin, ListView):
+    model = Character
