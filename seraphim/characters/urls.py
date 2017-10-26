6d3b6305ebcdc314faf80f68cb17062f6fe1a7f3
unknown <malaclypse2@gmail.com>
2017-10-25 21:50:37 -0400

    cont

--- seraphim/characters/urls.py
+++ seraphim/characters/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     # e.g.: /
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+    url(
+        regex=r'^$',
+        view=views.CharacterListView.as_view(),
+        name='index'
+    ),
     # e.g.: /characters/3/
     url(
         regex=r'^(?P<character_id>[0-9]+)/$',
--- seraphim/characters/views.py
+++ seraphim/characters/views.py
@@ -11,9 +11,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the characters index.")
 
-class CharacterDetailView(LoginRequiredMixin, DetailView):
+def CharacterDetailView(LoginRequiredMixin, DetailView):
     model = Character
     pk = 'character_id'
 
 class CharacterListView(LoginRequiredMixin, ListView):
     model = Character
+    
