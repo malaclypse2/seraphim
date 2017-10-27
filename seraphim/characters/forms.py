709b39c3295968a54033f522cfd31c121f301120
unknown <malaclypse2@gmail.com>
2017-10-26 22:01:44 -0400

    cont

--- seraphim/characters/forms.py
+++ seraphim/characters/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from .models import Character
 
 class CharacterForm(ModelForm):
@@ -11,3 +12,7 @@ class CharacterForm(ModelForm):
         self.helper = FormHelper()
         self.helper.form_id = 'id-CharacterForm'
         self.helper.form_class = 'Bootstrap4'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'characters:list'
+
+        self.helper.add_input(Submit('submit', 'Submit'))
