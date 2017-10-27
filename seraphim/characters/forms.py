0086976ec48ebc24346bd5a6cfba4089add9c232
unknown <malaclypse2@gmail.com>
2017-10-26 22:10:08 -0400

    basic

--- seraphim/characters/forms.py
+++ seraphim/characters/forms.py
@@ -4,15 +4,9 @@ from crispy_forms.layout import Submit
 from .models import Character
 
 class CharacterForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CharacterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('save', 'save'))
     class Meta:
         model = Character
-        fields = '__all__'
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id = 'id-CharacterForm'
-        self.helper.form_class = 'Bootstrap4'
-        self.helper.form_method = 'post'
-        self.helper.form_action = 'characters:list'
-
-        self.helper.add_input(Submit('submit', 'Submit'))
