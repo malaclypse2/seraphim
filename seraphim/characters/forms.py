5e5636d6d16cb6c5fe24bb2ffa15a9896c7e58de
unknown <malaclypse2@gmail.com>
2017-10-26 23:23:25 -0400

    hm

--- seraphim/characters/forms.py
+++ seraphim/characters/forms.py
@@ -1,13 +1,13 @@
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Div
 from .models import Character
 
 class CharacterForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(CharacterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.wrapper_class = 'row'
+        self.helper[:].wrap(Div, css_class='row')
         self.helper.label_class = 'col-sm-4'
         self.helper.field_class = 'col-sm-8'
         self.helper.add_input(Submit('submit', 'Save'))
