be5622651b428f31490d6e3ff6d4f0047d74d9b4
unknown <malaclypse2@gmail.com>
2017-10-26 21:23:27 -0400

    names are hard

--- seraphim/characters/urls.py
+++ seraphim/characters/urls.py
@@ -8,30 +8,30 @@ urlpatterns = [
     url(
         regex=r'^$',
         view=views.CharacterList.as_view(),
-        name='character_list'
+        name='list'
     ),
     # e.g.: /characters/3/
     url(
         regex=r'^(?P<pk>[0-9]+)/$',
         view=views.CharacterDetail.as_view(),
-        name='character_detail'
+        name='detail'
     ),
     # e.g.: /characters/create/
     url(
         regex=r'^create$',
         view=views.CharacterCreate.as_view(),
-        name='character_create'
+        name='create'
     ),
     # e.g.: /characters/update/3/
     url(
         regex=r'^update/(?P<pk>[0-9]+)/$',
         view=views.CharacterUpdate.as_view(),
-        name='character_update'
+        name='update'
     ),
     # e.g.: /characters/delete/3/
     url(
         regex=r'^delete/(?P<pk>[0-9]+)/$',
         view=views.CharacterDelete.as_view(),
-        name='character_delete'
+        name='delete'
     ),
 ]
