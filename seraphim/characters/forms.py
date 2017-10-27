1c43e4f2273408504efd99b0017c1e7a291b4182
unknown <malaclypse2@gmail.com>
2017-10-26 21:59:04 -0400

    stylin

--- seraphim/characters/forms.py
+++ seraphim/characters/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
 from .models import Character
 
 class CharacterForm(ModelForm):
     class Meta:
-        model = Character.
+        model = Character
         fields = '__all__'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-CharacterForm'
+        self.helper.form_class = 'Bootstrap4'
