a82092d0774034be4c351e9d080a57d5215d279b
unknown <malaclypse2@gmail.com>
2017-10-26 22:22:23 -0400

    horiz

--- seraphim/characters/forms.py
+++ seraphim/characters/forms.py
@@ -7,8 +7,9 @@ class CharacterForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(CharacterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.form_class='col-sm-4'
-        self.helper.field_class='col-sm-8'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-sm-4'
+        self.helper.field_class = 'col-sm-8'
         self.helper.add_input(Submit('submit', 'Submit'))
     class Meta:
         model = Character
