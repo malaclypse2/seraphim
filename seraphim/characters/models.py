3f9f174b9aa016b20cd5e1b6539797867b94fbe1
unknown <malaclypse2@gmail.com>
2017-10-28 17:52:17 -0400

    models

--- seraphim/characters/models.py
+++ seraphim/characters/models.py
@@ -3,23 +3,24 @@ from django.core.urlresolvers import reverse
 
 # Create your models here.
 class CharacterIcon(models.Model):
-    url = models.URLField()
+    url = models.CharField(max_length=200)
     def __str__(self):
         return self.url.rsplit('/', 1)[1]
 
 class Character(models.Model):
+    name = models.CharField(max_length=200)
+    # Meta information
+    owner = models.ForeignKey(
+        'users.user', on_delete=models.SET_NULL, null=True)
+    public = models.BooleanField(default=True)
     # Basic Character stats
     icon = models.ForeignKey(CharacterIcon, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200)
     profession = models.CharField('class', max_length=50)
     level = models.IntegerField()
     base_hp = models.IntegerField()
     av_short = models.CharField(max_length=50, default='AV: ')
     av_long = models.TextField(blank=True)
     campaign_status = models.TextField(blank=True)
-    # Meta information
-    owner = models.ForeignKey('users.user', False, null=True)
-    public = models.BooleanField(default=True)
     def __str__(self):
         return '{self.name}, {self.profession}:{self.level} - {self.base_hp} HP'.format(self=self)
     def get_absolute_url(self):
--- seraphim/tracker/admin.py
+++ seraphim/tracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Day, Combat, Round, StatusEffectType, StatusEffect, Heal, Wound
+admin.site.register(
+    [Day, Combat, Round, StatusEffectType, StatusEffect, Heal, Wound])
