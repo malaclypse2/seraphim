10148e97ff9715269c577082afa5fca476f12f02
unknown <malaclypse2@gmail.com>
2017-10-28 19:35:09 -0400

    more character editing

--- seraphim/characters/forms.py
+++ seraphim/characters/forms.py
@@ -1,13 +1,15 @@
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Div
+from crispy_forms.layout import Layout, Submit, HTML
+from crispy_forms.bootstrap import FormActions
+
 from .models import Character
 
 class CharacterForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(CharacterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.add_input(Submit('submit', 'Save'))
+        self.helper.form_tag = False
     class Meta:
         model = Character
         fields = '__all__'
