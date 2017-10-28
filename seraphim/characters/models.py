9ec90799dab23368b27e5851b8cf659fb2294834
unknown <malaclypse2@gmail.com>
2017-10-28 14:53:21 -0400

    charcter model update

--- seraphim/characters/models.py
+++ seraphim/characters/models.py
@@ -8,13 +8,15 @@ class CharacterIcon(models.Model):
         return self.url.rsplit('/', 1)[1]
 
 class Character(models.Model):
-    icon = models.ForeignKey(CharacterIcon)
+    icon = models.ForeignKey(CharacterIcon, False)
     name = models.CharField(max_length=200)
     profession = models.CharField('class', max_length=50)
     level = models.IntegerField()
     base_hp = models.IntegerField()
     av_short = models.CharField(max_length=50, default = 'AV: ')
     av_long = models.TextField(blank=True)
+    owner = models.ForeignKey('users.user', False, null=True)
+    public = models.BooleanField(default=True)
     def __str__(self):
         return '{self.name}, {self.profession}:{self.level} - {self.base_hp} HP'.format(self=self)
     def get_absolute_url(self):
