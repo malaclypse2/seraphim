92ce4b558ac10fceedf5f93c4d5708545779f398
unknown <malaclypse2@gmail.com>
2017-10-30 00:14:14 -0400

    hide the player and combat selection in wounds and healing

--- seraphim/tracker/forms.py
+++ seraphim/tracker/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, HTML
+from crispy_forms.layout import Layout, Submit, HTML, Field, Hidden, Div
 from crispy_forms.bootstrap import FormActions
 
 from .models import Combat, Heal, StatusEffect, StatusEffectType, Wound
@@ -11,6 +11,9 @@ class WoundForm(ModelForm):
         super(WoundForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_tag = False
+        # Hide the Combat and Player inputs, if this is a bound form
+        if self.is_bound:
+            self.helper[0:2].wrap(Div, css_class="d-none")
 
     class Meta:
         model = Wound
@@ -22,6 +25,8 @@ class HealForm(ModelForm):
         super(HealForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_tag = False
+        if self.is_bound:
+            self.helper[0:2].wrap(Div, css_class="d-none")
 
     class Meta:
         model = Heal
